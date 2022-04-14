1ad1a8bb793913e26221f506982a796f7795b4a7
sadmanfatin <sadmanfatin208@gmail.com>
2022-04-14 12:01:39 +0600

    added custom delete , find method in EmployeeRepo

--- src/main/java/com/example/EmployeeCrud/repo/EmployeeRepo.java
+++ src/main/java/com/example/EmployeeCrud/repo/EmployeeRepo.java
@@ -1,7 +1,14 @@
 package com.example.EmployeeCrud.repo;
 import com.example.EmployeeCrud.model.*;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
+	void deleteEmployeeById(Long id);
+
+	Optional<Employee> findEmployeeById(Long id);
+      
 }
